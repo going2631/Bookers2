18e262d3c4280f5d071551d414845975478ae89d
going2631 <tomo.moedora1212@gmail.com>
2021-05-12 07:31:05 +0000

    [Add] route追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
   def index
   end
-  
+
   def edit
   end
-  
+
   def create
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
-  def delete
-  end
+
 end
