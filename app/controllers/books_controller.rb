7cc277ddf532ccf671b566790bb525c3f327c6b2
going2631 <tomo.moedora1212@gmail.com>
2021-05-17 10:45:38 +0000

    [Add]book show edit修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,15 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = @book.user_id
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit
